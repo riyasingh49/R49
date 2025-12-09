bca6ebd15089ce965d292cd7f88fc0b968218055
riyasingh49 <riyasingh454247@gmail.com>
2025-12-09 20:57:56 +0530

    incomplete transition

--- src/pages/Agence.jsx
+++ src/pages/Agence.jsx
@@ -16,37 +16,54 @@ import image11 from "../assets/image11.avif";
 import image12 from "../assets/image12.avif";
 import image13 from "../assets/image13.avif";
 
-
 const Agence = () => {
   gsap.registerPlugin(ScrollTrigger);
-  const imageDivRef = useRef(null)
+  const imageDivRef = useRef(null);
+  const imageRef = useRef(null);
 
   const imageArray = [
-    image2, image3, image4, image5, image6, image7, image8, image9, image10, image11, image12 , image13
-
-  ]
-  useGSAP(function(){
-    gsap.to(imageDivRef.current,{
-      scrollTrigger:{
-        trigger:imageDivRef.current,
-        markers:true,
-        start:'top 28%',
-        end: 'top -70%',
-        pin:true,
-        onUpdate:()=>{
-          // const imageIndex = Math.floor(self.onprogress * imageArray.length)
-          console.log("hgf");
-          
-        }
-      }
-    })
-  })
+    image2,
+    image3,
+    image4,
+    image5,
+    image6,
+    image7,
+    image8,
+    image9,
+    image10,
+    image11,
+    image12,
+    image13,
+  ];
+  useGSAP(function () {
+    gsap.to(imageDivRef.current, {
+      scrollTrigger: {
+        trigger: imageDivRef.current,
+        // markers: true,
+        start: "top 28%",
+        end: "top -70%",
+        pin: true,
+        onUpdate: (elem) => {
+          let imageIndex;
+          if (elem.progress < 1) {
+            imageIndex = Math.floor(elem.progress * imageArray.length);
+          } else {
+            imageIndex = imageArray.length - 1;
+          }
+          imageRef.current.src = imageArray[imageIndex];
+        },
+      },
+    });
+  });
 
   return (
     <div>
       <div className="section1">
-        <div ref={imageDivRef} className="absolute overflow-hidden h-[20vw] w-[15vw] rounded-2xl  left-[30vw] top-60 bg-amber-50">
-          <img className="h-full object-cover w-full" src={image1} alt="" />
+        <div
+          ref={imageDivRef}
+          className="absolute overflow-hidden h-[20vw] w-[15vw] rounded-2xl  left-[30vw] top-60"
+        >
+          <img ref = {imageRef} className="h-full object-cover w-full" src={image1} alt="" />
         </div>
         <div className="relative font-[font2]">
           <div className=" mt-[55vh]">
@@ -55,9 +72,9 @@ const Agence = () => {
               Harmony
             </h1>
           </div>
-          <div className="pl-[45%] mt-[20]">
+          <div className="pl-[47%] mt-[20]">
             <p className="text-5xl">
-              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
               Step into a world shaped by nature’s gentle rhythm, where every
               moment feels alive, every breath carries calm, and every
               experience invites you to rediscover beauty, peace, and connection
@@ -66,9 +83,7 @@ const Agence = () => {
           </div>
         </div>
       </div>
-      <div className="section2 h-screen">
-
-      </div>
+      <div className="section2 h-screen"></div>
     </div>
   );
 };
