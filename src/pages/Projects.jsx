4dc626648ad17ffcacc4610d6bcf547357ff53b3
riyasingh49 <riyasingh454247@gmail.com>
2026-01-02 20:42:42 +0530

    images added and animation remained in explore section

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,25 +1,33 @@
 import React from "react";
-import explorePage1 from "../assets/explorePage1.avif";
+import ProjectCard from "../components/projects/ProjectCard";
+import explorePage1  from "../assets/explorePage1.avif";
+import explorePage2 from "../assets/explorePage2.avif";
+import explorePage3 from "../assets/explorePage3.avif";
+import explorePage4 from "../assets/explorePage4.avif";
+import explorePage5 from "../assets/explorePage5.avif";
+import explorePage6 from "../assets/explorePage6.avif";
+import explorePage7 from "../assets/explorePage7.avif";
+import explorePage8 from "../assets/explorePage8.avif";
+
+
 
 const Projects = () => {
+  const projects = [
+    {image1: explorePage1, image2: explorePage2},
+    { image1: explorePage3, image2: explorePage4},
+    {image1: explorePage5, image2: explorePage6},
+    {image1:explorePage7, image2: explorePage8}
+  ]
   return (
     <div className="p-4">
       <div className=" pt-[40vh]">
         <h2 className="font-[font2] text-[9.5vw] uppercase">Explore</h2>
       </div>
       <div className="-mt-10">
-        <div className="w-full h-[700px] mb-4 flex gap-4 ">
-          <div className="w-1/2 h-full group relative transition-all hover:rounded-[50px] overflow-hidden">
-            <img
-              className="h-full w-full object-cover"
-              src={explorePage1}
-              alt=""
-            />
-            <div className="opacity-0 transition-all group-hover:opacity-100 absolute flex items-center justify-center top-0 left-0 h-full w-full bg-black/10">
-            <h2 className="uppercase text-4xl font-[font1] pt-2 px-4 border-3 text-white border-white rounded-full">Emerald Grove</h2>
-            </div>
-          </div>
-        </div>
+        {projects.map(function(elem){
+          return <ProjectCard image1={elem.image1} image2={elem.image2}/>
+        })}
+
       </div>
     </div>
   );
