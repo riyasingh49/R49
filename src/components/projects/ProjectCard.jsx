2b1fde9e8dd9a75b715fa6f6c0168e64658a0269
riyasingh49 <riyasingh454247@gmail.com>
2026-01-21 00:09:04 +0530

    explore completed

--- src/components/projects/ProjectCard.jsx
+++ src/components/projects/ProjectCard.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
-import explorePage1 from "../../assets/explorePage1.avif";
 
 
 const ProjectCard = (props) => {
 
   return (
-    <div className="w-full h-[700px] mb-4 flex gap-4 ">
+         <>
           <div className="w-1/2 h-full group relative transition-all hover:rounded-[50px] overflow-hidden">
             <img className="h-full w-full object-cover" src={props.image1} alt="" />
             <div className="opacity-0 transition-all group-hover:opacity-100 absolute flex items-center justify-center top-0 left-0 h-full w-full bg-black/10">
@@ -18,7 +17,7 @@ const ProjectCard = (props) => {
             <h2 className="uppercase text-4xl font-[font1] pt-2 px-4 border-3 text-white border-white rounded-full">Emerald Grove</h2>
             </div>
           </div>
-        </div>
+         </>
   )
 }
 
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -8,24 +8,48 @@ import explorePage5 from "../assets/explorePage5.avif";
 import explorePage6 from "../assets/explorePage6.avif";
 import explorePage7 from "../assets/explorePage7.avif";
 import explorePage8 from "../assets/explorePage8.avif";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/all";
+import gsap from "gsap";
 
 
 
 const Projects = () => {
   const projects = [
     {image1: explorePage1, image2: explorePage2},
-    { image1: explorePage3, image2: explorePage4},
+    {image1: explorePage3, image2: explorePage4},
     {image1: explorePage5, image2: explorePage6},
     {image1:explorePage7, image2: explorePage8}
   ]
+
+  gsap.registerPlugin(ScrollTrigger)
+
+  useGSAP(function(){
+    gsap.from('.hero',{
+      height:"100px",
+      stagger:{
+        amount:0.5
+      },
+      scrollTrigger:{
+        trigger:".lol",
+        markers:true,
+        start:"top 100%",
+        end:"top -150%",
+        scrub:true
+      }
+    })
+  })
+
   return (
-    <div className="p-4">
+    <div className="p-4 mb-[40vh]">
       <div className=" pt-[40vh]">
         <h2 className="font-[font2] text-[9.5vw] uppercase">Explore</h2>
       </div>
-      <div className="-mt-10">
-        {projects.map(function(elem){
-          return <ProjectCard image1={elem.image1} image2={elem.image2}/>
+      <div className="-mt-10 lol">
+        {projects.map(function(elem,idx){
+          return <div key={idx} className="hero w-full h-[300px] mb-4 flex gap-4 " >
+          <ProjectCard image1={elem.image1} image2={elem.image2}/>
+          </div>
         })}
 
       </div>
